66a55e0343d50df80033c99f1fcfa2d9bd53ef09
amrutn <amrutn@berkeley.edu>
2019-12-19 11:42:36 -0800

    wore a rudimentary version, still needs to be tested

--- parse_gammas.py
+++ parse_gammas.py
@@ -24,7 +24,7 @@ def convert(k):
     return tuple(converted)
 
 
-def parse_gamma(gamma_output):
+def parse_gamma(gammas):
     """convert gamma output list from 
     extract_gammas_from_file(filename)
     the following list format:
@@ -32,12 +32,23 @@ def parse_gamma(gamma_output):
     J, K (0 for X, 1 for Y and 2 for Z) and for third dim, index 0 => gammaJJKK, index 1 => gamma JKJK and index 2 => gamma JKKJ
     """
     gamma_list = np.zeros(3,3,3)
-    for line in gamma_output:
-        
-    _, k, v = gamma_line.split()
-    k = convert(strip_gamma(k))
-    v = float(v)
-    return (k, v)
+    for gamma_line in gammas:
+        _, k, v = gamma_line.split()
+        k = convert(strip_gamma(k))
+        v = float(v)
+        if(k[0] == k[1] and k[1] == k[2] and k[2] == k[3]):
+            gamma_list[k[0]][k[0]][0] = v
+            gamma_list[k[0]][k[0]][1] = v
+            gamma_list[k[0]][k[0]][2] = v
+        elif (k[0] == k[1] and k[2] == k[3]):
+            gamma_list[k[0]][k[2]][0] = v
+        elif (k[0] == k[2] and k[1] == k[3]):
+            gamma_list[k[0]][k[1]][1] = v
+        elif (k[0] == k[3] and k[1] == k[2]):
+            gamma_list[k[0]][k[1]][2] = v
+        print(k)
+        print(v)
+    return gamma_list
 
 
 def extract_gammas_from_file(filename):
@@ -46,7 +57,8 @@ def extract_gammas_from_file(filename):
         gammas = []
         for line in lines:
             if "@ gamma(" in line:
-                gammas.append(parse_gamma(line))
+                gammas.append(line)
+        gammas = parse_gamma(gammas)
         return gammas
 
 if __name__ == "__main__":
