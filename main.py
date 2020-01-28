7b0ede5fc9ac16f1bd2f3c631e7939170dd3efa7
amrutn <amrutn@berkeley.edu>
2020-01-28 13:33:33 -0800

    tested program, correctly outputs second order hyperpolarizability in the lab frame and molecular frame

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ import parse_gammas
 
 
 def main():
-	gamma_list = parse_gammas.extract_gammas_from_file(argv[1]) 
-
+	gamma_list = parse_gammas.extract_gammas_from_file(argv[1])
 	print(gamma_list)
+	gamma_lab = parse_gammas.translate(gamma_list)
+	print(gamma_list)
+	print(gamma_lab)
 
 if __name__ == "__main__":
 	main()
--- parse_gammas.py
+++ parse_gammas.py
@@ -31,7 +31,7 @@ def parse_gamma(gammas):
     A 3x3x3 list with indeces,
     J, K (0 for X, 1 for Y and 2 for Z) and for third dim, index 0 => gammaJJKK, index 1 => gamma JKJK and index 2 => gamma JKKJ
     """
-    gamma_list = np.zeros(3,3,3)
+    gamma_list = np.zeros((3,3,3))
     for gamma_line in gammas:
         _, k, v = gamma_line.split()
         k = convert(strip_gamma(k))
@@ -46,8 +46,6 @@ def parse_gamma(gammas):
             gamma_list[k[0]][k[1]][1] = v
         elif (k[0] == k[3] and k[1] == k[2]):
             gamma_list[k[0]][k[1]][2] = v
-        print(k)
-        print(v)
     return gamma_list
 
 
@@ -60,7 +58,49 @@ def extract_gammas_from_file(filename):
                 gammas.append(line)
         gammas = parse_gamma(gammas)
         return gammas
+def delta(i, j):
+    """
+    Simulates Kroneker delta tensor
+    """
+    if i == j:
+        return 1
+    else:
+        return 0
+
+def translate(g):
+
+    """
+    converts gamma values from molecular frame to lab frame
+    input is a 3x3x3 array of gamma values 
+    indeces of g are J, K and for third dim, index 0 = gammaJJKK, index 1 = gamma JKJK and index 2 = gamma JKKJ
+
+    """
+
+    gammaJJKK = 0
+    for i in range(3):
+        for j in range(3):
+            gammaJJKK += g[i][j][0]   
+    gammaJKJK = 0
+    for i in range(3):
+        for j in range(3):
+            gammaJKJK += g[i][j][1]
+    gammaJKKJ=0
+    for i in range(3):
+        for j in range(3):
+            gammaJKKJ += g[i][j][2]            
+    gamma_lab = np.empty([3,3,3,3])
+    for i in range(3):
+        for j in range(3):
+            for k in range(3):
+                for l in range(3):
+                    term1 = gammaJJKK * (4 * delta(i, j) * delta(k, l) - delta(i, k) * delta(j, l) - delta(i, l) * delta(j, k))
+                    term2 = gammaJKJK * (4 * delta(i, k) * delta(j, l) - delta(i, j) * delta(k, l) - delta(i, l) * delta(j, k))
+                    term3 = gammaJKKJ * (4 * delta(i, l) * delta(j, k) - delta(i, k) * delta(j, l) - delta(i, j) * delta(k, l))
+                    gamma_lab[i][j][k][l] = (term1 + term2 + term3)/30
+    return gamma_lab
+
+
+                
 
 if __name__ == "__main__":
     gamma_output = extract_gammas_from_file(argv[1])
-    print(gamma_output)
